dfcc04967c7595dde46e52d03867db0c79265ae1
Vladislav <andreevvlad443@gmail.com>
2022-12-24 20:10:39 +0300

    MainActivity upd: implemented showNotification method

--- app/src/main/java/com/enzulode/notificationapp/MainActivity.kt
+++ app/src/main/java/com/enzulode/notificationapp/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.enzulode.notificationapp
 
+import android.annotation.SuppressLint
 import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.graphics.BitmapFactory
-import android.os.Build
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.widget.RemoteViews
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
@@ -67,15 +66,28 @@ class MainActivity : ComponentActivity()
 		}
 	}
 
+	@SuppressLint("UnspecifiedImmutableFlag")
 	private fun showNotification()
 	{
 		val notificationBuilder: NotificationCompat.Builder = NotificationCompat
 			.Builder(this, getString(R.string.notification_channel_id))
 
+		val collapsedView = RemoteViews(packageName, R.layout.notification_collapsed)
+		val expandedView = RemoteViews(packageName, R.layout.notification_expanded)
+		val headsupView = RemoteViews(packageName, R.layout.notification_headsup)
+
+		val clickIntent =  Intent(this, NotificationReceiver::class.java)
+		val clickPendingIntent = PendingIntent.getBroadcast(this, 0, clickIntent, 0)
+
+		collapsedView.setTextViewText(R.id.notification_collapsed_title, "New notification")
+		collapsedView.setOnClickPendingIntent(R.id.notification_collapsed_info, clickPendingIntent)
+
 		notificationBuilder
 			.setSmallIcon(R.drawable.ic_launcher_background)
-			.setContentTitle(getString(R.string.notification_title))
-			.setContentText("This is default notification")
+			.setCustomContentView(collapsedView)
+			.setCustomBigContentView(expandedView)
+			.setCustomHeadsUpContentView(headsupView)
+			.setOnlyAlertOnce(false)
 
 		notificationManagerCompat.notify(1, notificationBuilder.build())
 	}
