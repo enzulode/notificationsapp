06be82c338b5265fe1b9ce46ba51664b9521a56d
Vladislav <andreevvlad443@gmail.com>
2022-12-24 19:27:07 +0300

    main activity upd: implemented basic notifications

--- app/src/main/java/com/enzulode/notificationapp/MainActivity.kt
+++ app/src/main/java/com/enzulode/notificationapp/MainActivity.kt
@@ -1,21 +1,40 @@
 package com.enzulode.notificationapp
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.enzulode.notificationapp.ui.theme.NotificationAppTheme
 
 class MainActivity : ComponentActivity()
 {
+
+	private lateinit var builder: Notification.Builder
+	private lateinit var notificationManagerCompat: NotificationManagerCompat
+
+
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
+		notificationManagerCompat = NotificationManagerCompat.from(this)
+
+
 		setContent {
 			NotificationAppTheme {
 				Surface(
@@ -30,10 +49,34 @@ class MainActivity : ComponentActivity()
 						contentAlignment = Alignment.Center,
 					)
 					{
+
+						Button(
+							onClick = { showNotification() }
+						)
+						{
+							Text(
+								text = "Notify",
+								color = Color.Black
+							)
+						}
+
 					}
 
 				}
 			}
 		}
+	}
+
+	private fun showNotification()
+	{
+		val notificationBuilder: NotificationCompat.Builder = NotificationCompat
+			.Builder(this, getString(R.string.notification_channel_id))
+
+		notificationBuilder
+			.setSmallIcon(R.drawable.ic_launcher_background)
+			.setContentTitle(getString(R.string.notification_title))
+			.setContentText("This is default notification")
+
+		notificationManagerCompat.notify(1, notificationBuilder.build())
 	}
 }
