d19d0a784ef96f9548b98478cce9ef2cce179a08
Vladislav <andreevvlad443@gmail.com>
2022-12-24 18:05:58 +0300

    MainActiviry: removed unused imports

--- app/src/main/java/com/enzulode/notificationapp/MainActivity.kt
+++ app/src/main/java/com/enzulode/notificationapp/MainActivity.kt
@@ -7,11 +7,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.enzulode.notificationapp.ui.theme.NotificationAppTheme
 
 class MainActivity : ComponentActivity()
